d1cbbad32dd91d58069d61f82040f7ebe02de3b3
theZalmanian <TheZalmanian@gmail.com>
2022-11-18 12:43:18 -0800

    - Added methods to simulate d6 roll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 window.onload = function () {
+    setupButton("roll-die", rollD6);
 };
+function rollD6() {
+    var rolledValue = generateNumberWithinRange(1, 6);
+    getInputByID("output-score").value = rolledValue.toString();
+}
+function generateNumberWithinRange(min, max) {
+    var number = (Math.random() * max) + min;
+    return Math.floor(number);
+}
 function setupButton(id, useFunction) {
     var button = getByID(id);
     button.onclick = useFunction;
--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,31 @@
 window.onload = function():void {
-    
+    setupButton("roll-die", rollD6)
 }
+
+function rollD6() {
+    // simulate a six-sided dice roll by 
+    // generating a random number (1 - 6)
+    let rolledValue:number = generateNumberWithinRange(1, 6);
+    
+    // output result to page
+    getInputByID("output-score").value = rolledValue.toString();
+}
+
+/**
+ * Generates a random number within the given range
+ * @param min The lower limit of the range (inclusive)
+ * @param max The upper limit of the range (inclusive)
+ * @returns The generated number
+ */
+function generateNumberWithinRange(min:number, max:number):number {
+    // generate a random number between min and max
+    let number:number =  (Math.random() * max) + min;
+    
+    // as random generates a float, 
+    // return it without the decimals
+    return Math.floor(number);
+}
+
 
 /****************
 **** HELPERS ****
